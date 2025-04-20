49ba485e8809309948db07bc8dd0fdc91d208d77
jonathan-bug <jonathanlima.dev@gmail.com>
2025-04-20 12:41:46 -0600

    Added user view

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,12 +2,12 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8"/>
-        <title>@yield("title", "Kamui")</title>
+        <title>@yield("title", "KAMUI")</title>
         <link href="{{url('bootstrap/css/bootstrap.min.css')}}" rel="stylesheet"/>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         @stack("styles")
     </head>
-    <body>
+    <body class="bg-light">
         @yield("content")
         
         <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
---
+++ resources/views/pages/users/index.blade.php
@@ -0,0 +1,44 @@
+@extends("layout")
+
+@push("styles")
+<style>
+    body {
+        height: 100vh;
+    }
+
+    .container, .row {
+        height: inherit;
+    }
+</style>
+@endpush
+
+@section("content")
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-sm-10 col-md-6 col-lg-4 d-flex align-items-center">
+                <form class="card flex-grow-1" method="POST" action="{{route('users.login')}}">
+                    @csrf
+                    <div class="card-header">
+                        <div class="fw-bold fs-2 text-center">
+                            <i class="fa fa-yin-yang"></i>
+                            KAMUI
+                        </div>
+                    </div>
+                    <div class="card-body p-4">
+                        <div class="form-group">
+                            <label class="form-label" for="">Email</label>
+                            <input class="form-control" name="email" type="text" value=""/>
+                        </div>
+                        <div class="form-group mt-4">
+                            <label class="form-label" for="">Password</label>
+                            <input class="form-control" name="password" type="text" value=""/>
+                        </div>
+                    </div>
+                    <div class="card-footer d-flex justify-content-end p-4">
+                        <button class="btn btn-primary">Login</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
